714f0d148b7542c6e00f499ad301c02907b76fbf
Jacilinydasilva <107487066+jacilinydasilva@users.noreply.github.com>
2022-06-14 14:06:24 +0000

    initial push

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -8,8 +8,8 @@ function CourseCard(props) {
   return (
     <div>
       <Card style={{ width: "24rem", height: "470px", 
-  backgroundColor : props.course.Department == "English" ? "lightblue"      
-                  : props.course.Department == "History" ? "green" : }}>
+  backgroundColor : {  props.course.Department == "English" ? "lightblue"      
+                  : props.course.Department == "History" ? "green" : }}}>
         <Card.Body>
           <Card.Title>{props.course.Course_Name}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">{props.course.Course_Code}</Card.Subtitle>
